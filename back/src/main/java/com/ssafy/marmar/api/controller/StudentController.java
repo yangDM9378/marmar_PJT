5d1561260b45144361fb0f0f660e9ccb71d593ac
audwl910 <audwl910@naver.com>
2023-01-20 15:25:31 +0900

    style: 아이디 중복 검사 수정중

--- back/src/main/java/com/ssafy/marmar/api/controller/StudentController.java
+++ back/src/main/java/com/ssafy/marmar/api/controller/StudentController.java
@@ -2,7 +2,9 @@ package com.ssafy.marmar.api.controller;
 
 import com.ssafy.marmar.api.request.StudentRegisterPostReq;
 import com.ssafy.marmar.api.service.StudentService;
+import com.ssafy.marmar.api.service.TherapistService;
 import com.ssafy.marmar.db.model.Student;
+import com.ssafy.marmar.db.model.Therapist;
 import com.ssafy.marmar.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,15 +20,51 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
+    @Autowired
+    TherapistService therapistService;
+
+
+
     @GetMapping("/{studentId}")
     public ResponseEntity<Boolean> checkStudentId(@PathVariable String studentId) {
+
         try{
-            studentService.getUserByUserId(studentId);
+//            studentService.getUserByUserId(therapistId);
+            therapistService.getUserByUserId(studentId);
+
         }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+            try{
+                studentService.getUserByUserId(studentId);
+            } catch(NoSuchElementException e1){//아이디 중복되지 않으면 true 리턴
+                return ResponseEntity.status(200).body(true);
+            }
             return ResponseEntity.status(200).body(true);
         }
+
+//        try{
+//            studentService.getUserByUserId(therapistId);
+////            therapistService.getUserByUserId(therapistId);
+//        }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+//            return ResponseEntity.status(200).body(true);
+//        }
         //중복되면 false 리턴
         return	ResponseEntity.status(200).body(false);
+
+        //        try{
+//            studentService.getUserByUserId(studentId);
+////            therapistService.getUserByUserId(studentId);
+//        }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+//            return ResponseEntity.status(200).body(true);
+//        }
+//
+//        try{
+////            studentService.getUserByUserId(studentId);
+//            therapistService.getUserByUserId(studentId);
+//        }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+//            return ResponseEntity.status(200).body(true);
+//        }
+        //중복되면 false 리턴
+
     }
 
     @PostMapping()
--- back/src/main/java/com/ssafy/marmar/api/controller/TherapistController.java
+++ back/src/main/java/com/ssafy/marmar/api/controller/TherapistController.java
@@ -1,6 +1,7 @@
 package com.ssafy.marmar.api.controller;
 
 import com.ssafy.marmar.api.request.TherapistRegisterPostReq;
+import com.ssafy.marmar.api.service.StudentService;
 import com.ssafy.marmar.api.service.TherapistService;
 import com.ssafy.marmar.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +16,38 @@ import java.util.NoSuchElementException;
 public class TherapistController {
 
     @Autowired
+    StudentService studentService;
+
+    @Autowired
     TherapistService therapistService;
 
     @GetMapping("/{therapistId}")
-    public ResponseEntity<Boolean> checkStudentId(@PathVariable String therapistId) {
+    public ResponseEntity<Boolean> checkTherapistId(@PathVariable String therapistId) {
+//        System.out.println(studentService.getUserByUserId(therapistId));
+//        if(studentService.getUserByUserId(therapistId)==null && therapistService.getUserByUserId(therapistId)==null){
+//            return ResponseEntity.status(200).body(true);
+//        } else {
+//            return	ResponseEntity.status(200).body(false);
+//        }
         try{
+//            studentService.getUserByUserId(therapistId);
             therapistService.getUserByUserId(therapistId);
+
         }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+            try{
+                studentService.getUserByUserId(therapistId);
+            } catch(NoSuchElementException e1){//아이디 중복되지 않으면 true 리턴
+                return ResponseEntity.status(200).body(true);
+            }
             return ResponseEntity.status(200).body(true);
         }
+
+//        try{
+//            studentService.getUserByUserId(therapistId);
+////            therapistService.getUserByUserId(therapistId);
+//        }catch(NoSuchElementException e){//아이디 중복되지 않으면 true 리턴
+//            return ResponseEntity.status(200).body(true);
+//        }
         //중복되면 false 리턴
         return	ResponseEntity.status(200).body(false);
     }
