bc3ae2ed3e530afde356ff9bb9c931a0273deb1c
moonthree <fpdhslr0725@gmail.com>
2023-01-27 17:50:54 +0900

    feat: 그래들로 이주, openvidu cors 오류 발생

---
+++ front/src/api/userApi.js
@@ -0,0 +1,37 @@
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
+
+import axios from 'axios';
+import { authApi } from '../libs/axiosCofing';
+
+const signInApi = payload => authApi.post('auth/login', payload);
+
+const studentCheckApi = () =>
+  authApi('student/me').then(res => {
+    // console.log(res);
+    return res.data;
+  });
+
+const therapistCheckApi = () =>
+  authApi('therapist/me').then(res => {
+    // console.log(res);
+    return res.data;
+  });
+
+const signUpTherapistApi = payload => axios.post('therapist', payload);
+
+const signUpStudentApi = payload => axios.post('student', payload);
+
+const idCheckTherapistApi = payload => axios.get(`therapist/${payload}`);
+
+const idCheckStudentApi = payload => axios.get(`student/${payload}`);
+
+export {
+  signInApi,
+  studentCheckApi,
+  therapistCheckApi,
+  signUpTherapistApi,
+  signUpStudentApi,
+  idCheckTherapistApi,
+  idCheckStudentApi,
+};
---
+++ front/src/libs/axiosCofing.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+// axiosConfig
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080/api/v1';
+
+axios.defaults.baseURL = BASE_URL;
+// // 리소스 접근 허용
+// axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+// // 서로 다른 도메인간 쿠키 전달 허용
+// axios.defaults.withCredentials = true;
+
+export const authApi = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+authApi.interceptors.request.use(
+  function (config) {
+    // 요청이 전달되기 전에 작업 수행
+    const token = localStorage.getItem('token');
+
+    if (token !== 'undefined') {
+      config.headers.Authorization = token ? `Bearer ${token}` : null;
+    }
+    return config;
+  },
+  function (error) {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(error);
+  },
+);
