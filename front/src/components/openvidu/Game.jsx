ab20d98bad07e8810c0264b5c3e89c165c92633c
moonthree <fpdhslr0725@gmail.com>
2023-02-06 15:05:36 +0900

    feat: joinroom
    Former-commit-id: 4251d45d559825a2639b61886e3d1c038332b419

--- front/src/components/openvidu/Game.jsx
+++ front/src/components/openvidu/Game.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { SocketContext } from '../../context/SocketContext';
 import Program from '../../pages/onClass/Program';
 
-export default function Game() {
+export default function Game(props) {
+  const { sessionId } = props;
+
+  const { joinRoom } = useContext(SocketContext);
+
+  useEffect(() => {
+    joinRoom(sessionId);
+  }, []);
+
   return (
     <div>
       <Program />
