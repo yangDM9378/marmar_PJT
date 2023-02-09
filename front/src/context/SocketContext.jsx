9b15324eb912349f419f578dbe5cfa8ef4382b3b
LeeSuA <huimhuimi@naver.com>
2023-02-09 14:49:15 +0900

    fix: node url 변경
    Former-commit-id: bc6a0ade6eac44f4bd983261e1005a4f21e71b13

--- front/src/context/SocketContext.jsx
+++ front/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import { getQuestionApi } from '../api/programApi';
 
 export const SocketContext = createContext();
-export const socket = io.connect('i8c204.p.ssafy.io/node');
+export const socket = io.connect('http://localhost:4000'); // localhost:4000
 export default function SocketProvider({ children }) {
   const [roomName, setRoomName] = useState('');
 
