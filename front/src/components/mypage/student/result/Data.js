f9f871430dba3c24d5b8243ff0a035308f934728
jiaekim <jiae1625@gmail.com>
2023-02-10 14:02:11 +0900

    feat: evaluation line chart
    Former-commit-id: ed77f9474872c8b9219fc7ca9a384d7884800cec

---
+++ front/src/components/mypage/student/result/Data.js
@@ -0,0 +1,32 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 50000,
+    userLost: 823,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 8000,
+    userLost: 823,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 10000,
+    userLost: 823,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 5000,
+    userLost: 823,
+  },
+];
--- front/src/components/mypage/student/result/Result.jsx
+++ front/src/components/mypage/student/result/Result.jsx
@@ -1,27 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { getResultApi } from '../../../../api/mypageApi';
-import useAuth from '../../../../hooks/queries/useAuth';
+import ShowResult from './ShowResult';
 
 export default function Result() {
-  const { useStudentCheck } = useAuth();
-  const { data: student } = useStudentCheck();
-  const { data: result } = useQuery({
-    queryKey: ['result', student.num],
-    queryFn: () => getResultApi(student.num),
-    onSuccess: res => {
-      console.log(res.data);
-    },
-    onError: () => {},
-  });
-  const handleSubmit = () => {};
   return (
     <div>
-      {result && <div>{result.data[0].num}</div>}
-      <button type="button" onClick={handleSubmit}>
-        {' '}
-        button
-      </button>
+      <ShowResult />
     </div>
   );
 }
