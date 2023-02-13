7aac19437aa90762c5b5ad4b2f975b26614a9fc7
LeeSuA <huimhuimi@naver.com>
2023-02-13 16:59:11 +0900

    fix: 데이터 삽입
    Former-commit-id: b54e3fcdd69dd12307578a3acbca1d2cce7da3d0

--- front/src/index.js
+++ front/src/index.js
@@ -135,7 +135,7 @@ const router = createBrowserRouter([
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={router} />);
-registerServiceWorker();
+// registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
